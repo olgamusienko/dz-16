c41c1192d7703120c15db27f5bd6fe183fd3a6f9
olgamusienko <ogamusienko1@gmail.com>
2023-07-06 21:01:21 +0200

    Added test classes for Booking API testing

---
+++ src/test/java/CreateAuthorizationBody.java
@@ -0,0 +1,13 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreateAuthorizationBody {
+    private String username;
+    private String password;
+}
